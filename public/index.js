45ead0e2d1ed871984aeee9e2ab74b808ac74ed0
Yuki-Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2022-12-08 15:27:52 +0900

    プレイヤーの移動と雪玉を投げれるようにした

--- public/index.js
+++ public/index.js
@@ -1,6 +1,9 @@
 const mapImage = new Image();
 mapImage.src = '/snowy-sheet.png';
 
+const santaImage = new Image();
+santaImage.src = '/santa.png';
+
 const canvasEl = document.getElementById('canvas');
 canvasEl.width = window.innerWidth;
 canvasEl.height = window.innerHeight;
@@ -10,20 +13,88 @@ const socket = io(`ws://localhost:5000`);
 
 const TILE_SIZE = 32; // 動画だと16だったけど画像のサイズ的に合わないので大きめに
 const TILES_IN_ROW = 8;
-let map = [[]];
+const SNOWBALL_SIZE = 5;
 
+let myId = null;
+let map = [[]];
+let players = [];
+let snowballs = [];
+
+// socket接続成功
 socket.on('connect', () => {
   console.log('connected');
+  myId = socket.id;
+  console.log(myId);
 });
 
+// サーバーからmapデータを受信
 socket.on('map', (loadedMap) => {
   map = loadedMap;
-  console.log(map)
+});
+
+// サーバーからプレイヤー情報を受信
+socket.on('players', (serverPlayers) => {
+  players = serverPlayers; 
+});
+
+// サーバーから雪玉情報を受信
+socket.on('snowballs', (serverSnowballs) => {
+  snowballs = serverSnowballs; 
+});
+
+const inputs = {
+  up: false,
+  down: false,
+  left: false,
+  right: false,
+};
+
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'w') {
+    inputs['up'] = true;
+  } else if (e.key === 's') {
+    inputs['down'] = true;
+  } else if (e.key === 'a') {
+    inputs['left'] = true;
+  } else if (e.key === 'd') {
+    inputs['right'] = true;
+  }
+  socket.emit('inputs', inputs);
+});
+
+window.addEventListener('keyup', (e) => {
+  if (e.key === 'w') {
+    inputs['up'] = false;
+  } else if (e.key === 's') {
+    inputs['down'] = false;
+  } else if (e.key === 'a') {
+    inputs['left'] = false;
+  } else if (e.key === 'd') {
+    inputs['right'] = false;
+  }
+  socket.emit('inputs', inputs);
+});
+
+window.addEventListener('click', (e) => {
+  const angle = Math.atan2(e.clientY - canvasEl.height / 2, e.clientX - canvasEl.width / 2);
+  socket.emit('snowball', angle);
 });
 
 function loop () {
-  canvas.clearRect(0, 0, canvas.width, canvas.height);
+  canvas.clearRect(0, 0, canvasEl.width, canvasEl.height);
 
+  // 自キャラを取得
+  const myPlayer = players.find((player) => player.id === socket.id);
+
+  // 自キャラが入ればカメラの位置を自キャラが真ん中に来るように調整する
+  let cameraX = 0;
+  let cameraY = 0;
+  if (myPlayer) {
+    cameraX = parseInt(myPlayer.x - canvasEl.width / 2);
+    cameraY = parseInt(myPlayer.y - canvasEl.height / 2);
+  }
+
+  // マップを描画
   for (let row = 0; row < map.length; row++) {
     for (let col = 0; col < map[0].length; col++) {
       const { id } = map[row][col];
@@ -36,14 +107,29 @@ function loop () {
         imageRow * TILE_SIZE,
         TILE_SIZE,
         TILE_SIZE,
-        col * (TILE_SIZE / 2), // 画像が大きくて配置するときのサイズ感が合わないので配置のサイズは調整する
-        row * (TILE_SIZE / 2),
-        TILE_SIZE / 2,
-        TILE_SIZE / 2
+        col * TILE_SIZE - cameraX,
+        row * TILE_SIZE - cameraY,
+        TILE_SIZE,
+        TILE_SIZE
       );
     }
   }
+
+  // キャラクターを描画
+  for (const player of players) {
+    canvas.drawImage(santaImage, player.x - cameraX, player.y - cameraY);
+  }
+
+  // 雪玉描画
+  for (const snowball of snowballs) {
+    canvas.fillStyle = '#fff';
+    canvas.beginPath();
+    canvas.arc(snowball.x - cameraX, snowball.y - cameraY, SNOWBALL_SIZE, 0, 2 * Math.PI);
+    canvas.fill();
+  }
+
   window.requestAnimationFrame(loop);
 }
 
 window.requestAnimationFrame(loop);
+
--- src/index.js
+++ src/index.js
@@ -9,18 +9,120 @@ const io = new Server(httpServer);
 
 const loadMap = require('./mapLoader');
 
+const SPEED = 5;
+const TICK_RATE = 30;
+const SNOWBALL_SPEED = 7;
+const PLAYER_SIZE = 32;
+
+const inputMap = {};
+let players = [];
+let snowballs = [];
+
+/**
+ * ループで実行される処理
+ * @params {Date} delta
+ */
+function tick (delta) {
+  // プレイヤーのの位置計算
+  for (const player of players) {
+    const inputs = inputMap[player.id];
+    if (inputs.up) {
+      player.y -= SPEED;
+    } else if (inputs.down) {
+      player.y += SPEED;
+    }
+    if (inputs.left) {
+      player.x -= SPEED;
+    } else if (inputs.right) {
+      player.x += SPEED;
+    }
+  }
+
+  // 雪玉の位置計算
+  for (const snowball of snowballs) {
+    snowball.x += Math.cos(snowball.angle) * SNOWBALL_SPEED;
+    snowball.y += Math.sin(snowball.angle) * SNOWBALL_SPEED;
+    snowball.timeLeft -= delta;
+
+    // 当たり判定
+    for (const player of players) {
+      if (player.id === snowball.playerId) continue; // 自キャラは無視
+      const distance = Math.sqrt((player.x + PLAYER_SIZE / 2 - snowball.x)**2 + (player.y + PLAYER_SIZE / 2 - snowball.y)**2);
+      if (distance <= PLAYER_SIZE / 2) {
+        player.x = 0;
+        player.y = 0;
+        snowball.timeLeft = -1; // 当たった雪玉も消す
+      }
+    }
+  }
+  snowballs = snowballs.filter((snowball) => snowball.timeLeft > 0); // 不要な雪玉を削除
+
+  io.emit('players', players);
+  io.emit('snowballs', snowballs);
+}
+
+/**
+ * メイン処理
+ */
 async function main () {
   // サーバー起動時にマップを取得
   const map2D = await loadMap();
 
   io.on('connect', (socket) => {
     console.log('socket', socket.id);
+
+    // プレイヤーを追加
+    players.push({
+      id: socket.id,
+      x: 0,
+      y: 0,
+    });
+
+    // プレイヤーごとの入力の初期値を設定
+    inputMap[socket.id] = {
+      up: false,
+      down: false,
+      left: false,
+      right: false,
+    };
+
+    // マップデータをクライアントに送信
     socket.emit('map', map2D);
+
+    // 入力を受け取ったら更新
+    socket.on('inputs', (inputs) => {
+      inputMap[socket.id] = inputs; // ユーザーごとの入力を保存
+    });
+
+    // 雪玉を投げるアクションを受信
+    socket.on('snowball', (angle) => {
+      const player = players.find((player) => player.id === socket.id);
+      snowballs.push({
+        angle,
+        x: player.x,
+        y: player.y,
+        timeLeft: 1000,
+        playerId: socket.id,
+      })
+    });
+
+    // 接続が切れたときにユーザーを削除
+    socket.on('disconnect', () => {
+      players = players.filter((player) => player.id !== socket.id);
+    });
   });
 
   app.use(express.static('public'));
 
   httpServer.listen(5000);
+
+  let lastUpdate = Date.now();
+  setInterval(() => {
+    const now = Date.now();
+    const delta = now - lastUpdate;
+    tick(delta);
+    lastUpdate = now;
+  }, 1000 / TICK_RATE);
 }
 
 main();
